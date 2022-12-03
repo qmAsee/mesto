bef2c17523e70dafa3664e53f6bee9e34435d182
Maxim Kupchin <max.kup.gis@yandex.ru>
2022-12-04 02:27:39 +0500

    1st deploy

---
+++ scripts/index.js
@@ -0,0 +1,38 @@
+let editProfile = document.querySelector('.profile__edit');
+let popup = document.querySelector('.popup');
+let popupClose = document.querySelector('.popup__close');
+let popupBody = document.querySelector('.popup__body');
+let popupName = document.querySelector('.popup__input_name');
+let popupProfession = document.querySelector('.popup__input_profession');
+let profileName = document.querySelector('.profile__name');
+let profileProfession = document.querySelector('.profile__description');
+let editSave = document.querySelector('.popup__content');
+
+editProfile.addEventListener('click', openPopup);
+popupClose.addEventListener('click', closePopup);
+
+function openPopup() {
+  popup.classList.add('popup_opened');
+  popupName.value = profileName.innerText;
+  popupProfession.value = profileProfession.innerText;
+}
+
+function closePopup() {
+    popup.classList.remove('popup_opened');
+}
+
+popup.addEventListener('click', function(event) {
+  if(event.target == popupBody) {
+    closePopup();
+  }
+});
+
+function handleFormSubmit(event) {
+  event.preventDefault();
+  profileName.textContent = popupName.value;
+  profileProfession.textContent = popupProfession.value;
+  closePopup();
+}
+
+editSave.addEventListener('submit', handleFormSubmit);
+
